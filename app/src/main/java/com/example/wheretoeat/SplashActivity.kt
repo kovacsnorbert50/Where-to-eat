324f14082ce71208c847ec1087a2d6ca55baba4f
kovacsnorbert50 <kovacsnorbert50@yahoo.com>
2021-02-06 19:42:57 +0200

    FINISH

--- app/src/main/java/com/example/wheretoeat/SplashActivity.kt
+++ app/src/main/java/com/example/wheretoeat/SplashActivity.kt
@@ -3,8 +3,15 @@ package com.example.wheretoeat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.wheretoeat.repository.Repository
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -12,5 +19,6 @@ class SplashActivity : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
+
     }
 }
